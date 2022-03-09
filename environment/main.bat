f6d7e6f0547eaa039d9bbc602312e9ff15d970ca
Thisguy217 <87245214+Thisguy217@users.noreply.github.com>
2022-03-09 11:03:46 -0700

    Create main.bat

---
+++ environment/main.bat
@@ -0,0 +1,23 @@
+@echo off
+
+color 02
+
+echo Prepping raw data...
+
+timeout 5
+
+ren *.csv Work_Emails_Raw.csv
+
+echo Raw Data prepped for refinement
+
+echo Beginning data manipulation
+
+echo excuting Python Script
+
+python CSV_Work_Parser.py
+
+echo Finishing...
+
+echo File output to location: %cd%
+
+pause
