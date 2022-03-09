b1becfd49816aeff06e876dba741e556fd91fe0f
Thisguy217 <87245214+Thisguy217@users.noreply.github.com>
2022-03-09 11:58:48 -0700

    Create main.sh

---
+++ environment/main.sh
@@ -0,0 +1,19 @@
+echo "$(tput setaf 2) Prepping raw data..."
+
+timeout 5
+
+ren *.csv Work_Emails_Raw.csv
+
+echo "Raw Data prepped for refinement"
+
+echo "Beginning data manipulation"
+
+echo "Excuting Python Script"
+
+python3 CSV_Work_Parser.py
+
+echo "Finishing..."
+
+set location 
+
+echo "File output to location: $(tput setaf 1) $(pwd) $(tput sgr 0)" 
